834f6d3705a7b9717765dc8cd7388dcb11aaf44f
shiwasmi <sagarchattar@gmail.com>
2026-05-12 07:33:28 +0530

    Terraform  Code

--- modules/s3bucket/resource.tf
+++ modules/s3bucket/resource.tf
@@ -1,4 +1,4 @@
-resource "aws_s3_bucket_acl" "mybucket" {
+resource "aws_s3_bucket" "mybucket" {
   bucket = var.bucket_name
-  acl    = var.bucket_privay
+  acl    = var.bucket_acl
 }
--- modules/s3bucket/variable.tf
+++ modules/s3bucket/variable.tf
@@ -2,10 +2,6 @@ variable "bucket_name" {
   type = string
 }
 
-variable "bucket_privay" {
-  type    = string
-  default = null
-}
 variable "bucket_acl" {
   type    = string
   default = "private"
