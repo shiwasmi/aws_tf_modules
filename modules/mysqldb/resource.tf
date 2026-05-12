d34610c5cb853137b4fdea3ed7ca17b61b0be544
shiwasmi <sagarchattar@gmail.com>
2026-05-12 10:38:34 +0530

    Terraform  Code

--- modules/mysqldb/resource.tf
+++ modules/mysqldb/resource.tf
@@ -1,6 +1,6 @@
 resource "aws_db_subnet_group" "this" {
   name       = "${var.db_name}-subnet-group"
-  subnet_ids = var.subnet_ids # must be declared in variables.tf
+  subnet_ids = var.subnet_ids
   tags = {
     Name = "${var.db_name}-subnet-group"
   }
@@ -10,7 +10,7 @@ resource "aws_db_instance" "this" {
   identifier             = var.db_name
   allocated_storage      = 20
   engine                 = "mysql"
-  engine_version         = "8.0" # optional, but recommended
+  engine_version         = "8.0"
   instance_class         = var.instance_class
   db_name                = var.db_name
   username               = var.db_username
